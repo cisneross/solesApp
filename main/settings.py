88e5f55d1467d325379b21c1cdff599ba8132f97
Sergio D. Cisneros <cisneross2217@gmail.com>
2018-10-24 13:03:34 -0700

    next commit

--- main/settings.py
+++ main/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'main/media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
